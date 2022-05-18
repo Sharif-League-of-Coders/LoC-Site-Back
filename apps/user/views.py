2a85d20260fb74a3d07134ebd2b56bb056410417
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2022-05-18 05:07:11 +0430

    Fix wrong import

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate
 from django.utils import timezone
