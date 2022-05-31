eb181b42660dc0152023706f4c50a7d24d7eea69
Hossein-AliHosseini <themmdhossein@gmail.com>
2022-05-31 11:22:19 +0430

    Change models admin page

--- apps/team/admin.py
+++ apps/team/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db import models
 
 from .models import Team, Invitation
 
-admin.site.register(Team)
+
+class InvitationInline(admin.StackedInline):
+    model = Invitation
+
+
+@admin.register(Team)
+class TeamAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'creator', )
+    search_fields = ('name', 'creator', )
+    list_editable = ('name', )
+    # list_filter = ()
+    inlines = (InvitationInline, )
+
+
 admin.site.register(Invitation)
+
+
+@admin.register(Invitation)
+class InvitationAdmin(ModelAdmin):
+    list_display = ('id', 'user', 'team', 'type', 'status')
+    list_filter = ('type', 'status')
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import User, Person
 
-admin.site.register(User)
-admin.site.register(Person)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'username', 'is_active', 'is_superuser', 'persono', 'team']
+    # list_editable = []
+    # list_display_links = []
+    search_fields = ['email']
+    # sortable_by = []
+    # list_filter = []
+
+@admin.register(Person)
+class ProfileAdmin(ModelAdmin):
+    # inlines = (SkillInline, JobExperienceInline)
+    list_display = ('id', 'firstname', 'lastname', 'birthdate', 'phonenumber',
+                    'stu_number', 'user')
+
+    # list_filter = ('university', 'major', 'university_degree')
+
+    search_fields = ('firstname', 'lastname',
+                     'phonenumber', )
